7790a1316d1616060f509cc16caf44602d1f6eff
GameXost <dsergey010206@gmail.com>
2026-01-01 01:52:00 +0300

    init

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/GameXost/wbTestCase.git
+module github.com/GameXost/wbTestCase
 
 go 1.25.4
--- internal/repository/bruh.go
+++ internal/repository/bruh.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"context"
-	"github.com/GameXost/wbTestCase/models"
 )
 
 func GetOrder(ctx context.Context, OrderUId string) models.Order {
