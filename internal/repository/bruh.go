f099502c5c2dc7b29864cc98be2f26ab65edcdfc
GameXost <dsergey010206@gmail.com>
2026-01-01 03:23:10 +0300

    hate this shit

--- internal/repository/bruh.go
+++ internal/repository/bruh.go
@@ -2,8 +2,19 @@ package repository
 
 import (
 	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type Repo struct {
+	pool *pgxpool.Pool
+}
+
+func NewRepo(pool *pgxpool.Pool) *Repo {
+	return &Repo{pool: pool}
+}
 func GetOrder(ctx context.Context, OrderUId string) models.Order {
-	return models.Order{}
+}
+
+func CreateOrder() {
+
 }
